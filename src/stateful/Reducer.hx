322db4d758e1525c3dc3ee2b69d5fb7ed16b4f71
kevinresol <kevinresol@gmail.com>
2016-10-27 15:35:10 +0800

    Return original state if unchanged

--- src/stateful/Reducer.hx
+++ src/stateful/Reducer.hx
@@ -3,6 +3,8 @@ package stateful;
 import haxe.macro.Expr;
 import haxe.macro.Context;
 
+using Lambda;
+
 typedef R<S, A> = S->A->S; 
 
 @:callable
@@ -11,13 +13,27 @@ abstract Reducer<S, A>(R<S, A>) from R<S, A> to R<S, A> {
 		switch e.expr {
 			case EObjectDecl(fields):
 				var rets = [];
+				var conds = [];
 				for(field in fields) {
 					var name = field.field;
 					var expr = field.expr;
 					rets.push({field: name, expr: macro $expr(state == null ? null : state.$name, action)});
+					conds.push(macro state.$name != newState.$name);
 				}
 				var ret = {expr: EObjectDecl(rets), pos: e.pos};
-				return macro function(state, action) return $ret;
+				return macro function(state, action) {
+					var newState = $ret;
+					
+					// return newState if original state is null
+					if(state == null) return newState;
+					
+					// return newState if any of the sub-state changed
+					if(${conds.fold(function(i, last) return last == null ? i : macro $last || $i, null)})
+						return newState;
+					
+					// otherwise return the original state
+					return state;
+				}
 				
 			default:
 				Context.error('Expected object declaration', e.pos);
